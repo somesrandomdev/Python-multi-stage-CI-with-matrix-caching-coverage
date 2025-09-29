9537db57de9616b92a7934045cc4089907b01f2e
somesrandomdev <ibrahimakhaliloullahndiaye@gmail.com>
2025-09-29 11:42:38 +0000

    deploy2

--- test_main.py
+++ test_main.py
@@ -1,4 +1,8 @@
-from main import hello
+from main import app  # import the Flask app
 
-def test_hello():
-    assert hello() == "Hello, world!"
+def test_alive():
+    with app.test_client() as c:
+        rv = c.get("/")
+        json = rv.get_json()
+        assert rv.status_code == 200
+        assert "I’m alive" in json["message"]
